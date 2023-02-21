4b54f3bd74d253106c45912fed63a8e2fd75f9b5
Sojib Mondol <mdsojibmandol@gmail.com>
2023-02-21 23:53:37 +0600

    Basic server done with database connectivity

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zmcxwrx.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+async function run(){
+    try{
+        
+    }
+    finally{
+
+    }
+}
+run().catch(console.log)
+
+
+
+
+app.get('/', async(req, res) =>{
+    res.send('Freelancer Nation server is running');
+})
+app.listen(port, () => console.log(`Freelancer Nation server running on ${port}`))
