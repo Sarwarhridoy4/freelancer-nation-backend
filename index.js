f69a430d586f85eab0d106f39ce638b426cdd011
Sojib Mondol <mdsojibmandol@gmail.com>
2023-02-22 23:26:23 +0600

    guidelineData & serviceCatogory

--- index.js
+++ index.js
@@ -10,15 +10,36 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zmcxwrx.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0opjg0j.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-
+console.log(uri);
 
 
 async function run(){
     try{
-        
+         // guidelineData
+         const guidelineData = client.db('freelancerNation').collection('guidelineData');
+         const serviceCategoty = client.db('freelancerNation').collection('serviceCategoty');
+
+
+
+
+         // gettung guidelineData
+        app.get('/guidelineData', async(req, res) => {
+            const query = {};
+            const data = await guidelineData.find(query).toArray();
+            res.send(data);
+        });
+
+        // gettung serviceCategoty
+        app.get('/serviceCategoty', async(req, res) => {
+            const query = {};
+            const data = await serviceCategoty.find(query).toArray();
+            res.send(data);
+        })
+
+
     }
     finally{
 
