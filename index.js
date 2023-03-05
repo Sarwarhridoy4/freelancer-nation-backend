ae60311882d1af1d39c7f29154db917000e2e544
Sojib Mondol <mdsojibmandol@gmail.com>
2023-03-05 11:18:21 +0600

    code bug fixed

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 // code for chat
-const http = require("http");
-const path = require("path");
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
-const portchat = process.env.PORT || 4000;
+
 
 const app = express();
 
@@ -80,47 +78,6 @@ run().catch(console.log)
 app.get('/', async(req, res) =>{
     res.send('Freelancer Nation server is running');
 })
-//code for chat
 
-// Serve the React frontend
-app.use(express.static(path.join(__dirname, "client/build")));
-
-// Start the server
-const server = app.listen(portchat, () => {
-  console.log(`Chat server up and running on port ${portchat}`);
-});
-
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "https://freelancernation.web.app",
-  },
-});
-
-// Keep track of the messages that have already been sent
-let sentMessages = [];
-
-// Handle Socket.IO connections
-io.on("connection", (socket) => {
-  console.log(`New client connected with: ${socket?.id}`);
-
-  // Send the messages that have already been sent to the new client
-  sentMessages.forEach((message) => {
-    socket.emit("message", message);
-  });
-
-  socket.on("message", (message) => {
-    // Check if the message has already been sent
-    if (!sentMessages.includes(message)) {
-      // If the message hasn't been sent, add it to the list of sent messages
-      sentMessages.push(message);
-      // Broadcast the message to all other clients
-      socket.broadcast.emit("message", message);
-    }
-  });
-
-  socket.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
-});
 
 app.listen(port, () => console.log(`Freelancer Nation server running on ${port}`))
